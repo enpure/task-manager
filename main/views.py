f42a3da87ee960232e90b8ce00947e33e121487c
unknown <nochden7@gmail.com>
2023-10-29 12:53:59 +0700

    усе готво

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
@@ -10,8 +10,18 @@ def about(request):
     return render(request, 'main/about.html')
 
 def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
     form = TaskForm()
     context = {
-        'form': form
+        'form': form,
+        'error': error
     }
     return render(request, 'main/create.html', context)
